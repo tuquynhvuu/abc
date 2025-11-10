07c7a8ac4f6e8ed5c2ad8314663cfb7b5909cef1
tuquynhvuu <aqv9882@nyu.edu>
2025-11-10 23:34:17 +0800

    fixed

--- maplight-ver5/public/sketch.js
+++ maplight-ver5/public/sketch.js
@@ -310,7 +310,7 @@ let blocks = [
 {
   name: "easteast1",
   corners:[
-    {lat:31.149568, lon:121.483281}, //top left
+    {lat:31.148314, lon:121.482358}, //top left
     {lat:31.148847, lon:121.483410}, // bottom left
     {lat:31.148985, lon:121.484246}, //bottom right
     {lat:31.149695, lon:121.484069} //top right
